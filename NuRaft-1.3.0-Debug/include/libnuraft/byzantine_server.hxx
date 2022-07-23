da7639f3f8159a3a8482ba8609bb06432fb24cff
BozeBro <ben.ozua@gmail.com>
2022-07-23 00:54:17 -0400

    removing redundant code

--- NuRaft-1.3.0-Debug/include/libnuraft/byzantine_server.hxx
+++ NuRaft-1.3.0-Debug/include/libnuraft/byzantine_server.hxx
@@ -9,11 +9,9 @@ namespace nuraft {
             byz_server(context* ctx, const init_options& opt = init_options(), bool verbose = false);
             ptr<resp_msg> process_req(req_msg& req) override;
         protected:
-            void handle_prevote_resp(resp_msg& resp);
-            void initiate_vote(bool force_vote = false);
-            void request_vote(bool force_vote);
-            void handle_vote_resp(resp_msg& resp);
+            void handle_vote_resp(resp_msg& resp) override;
             void handle_peer_resp(ptr<resp_msg>& resp, ptr<rpc_exception>& err) override;
+            void handle_prevote_resp(resp_msg& resp) override;
     };
 }
 #endif // _BYZANTINE_SERVER_HXX_
